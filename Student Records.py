aff6f0c777c3f23dc323ef076849ed07c7de3e4a
Parth Khajgiwale <90266917+parthkhajgiwale@users.noreply.github.com>
2022-10-25 10:00:44 +0530

    Student Records.py

--- Student Records.py
+++ Student Records.py
@@ -1,80 +1,51 @@
-n = int(input("Enter the number of students-\n"))
-marks = []
-for i in range(n):
-    element = int(input("Enter the marks-\n"))
-    marks.append(element)
-    
-def average(marks):
-    sum = 0
-    len = 0
-    for i in marks:
-        if i != -1:
-            sum += i
-    for j in marks:
-        if j != -1:
-            len += 1
-    return sum/len
+marks=[]
+print("Fundamentals of Data Structures Marks")
+n=int(input("Enter No. Of Students: "))
 
-def maximum(marks):
-    max = -9999999
-    for i in marks:
-        if i == -1:
-            pass
-        elif i > max:
-            max = i
-        else:
-            pass
-    return max
+def read():
+	for i in range(0,n):
+		num=int(input("Enter Marks:"))
+		marks.append(num)
+def display():
+	print(marks)
+def main():
+	read()
+	display()
+def avg():
+	sum=0
+	present=0
+	for  i in range(0,n):
+		if(marks[i]>=0):
+			sum+=marks[i]
+			present+=1
+	avg=sum/present
+	print("Average Marks of Class: ",avg)
+	print("Absent Students: ",n-present)
+def max_score():
+	max=0
+	for i in range(0,n):
+		if((marks[i]>=0) and (marks[i]>max)):
+			max=marks[i]
+	print("Maximum Mark : ",max)
+def min_score():
+	min_score=[]
+	for i in marks:
+		if(i>=0):
+			min_score.append(i)
+	print("Minimum Mark : ", min(min_score))
+def frequent_score():
+	max=0
+	hf_value=0
+	for i in marks:
+		frequency=marks.count(i)
+		if frequency>max and i>=0:
+			max=frequency
+			hf_value=i
+	print("Mark with Highest Frequency: ",hf_value )
 
-def minimum(marks):
-    min = 9999999
-    for i in marks:
-        if i == -1:
-            pass
-        elif i < min:
-            min = i
-        else:
-            pass
-    return min
 
-def max_frequency(marks):
-    distinct_elements = []
-    count_of_distinct_elements = []
-    for i in marks:
-        if i not in distinct_elements and i!=-1:
-            distinct_elements.append(i)
-        else:
-            pass
-    for j in distinct_elements:
-        count = 0
-        for k in marks:
-            if j == k:
-                count += 1
-            else:
-                pass
-        count_of_distinct_elements.append(count)
-    max = -9999999
-    for l in count_of_distinct_elements:
-        max_index = 0
-        if l > max or l == max:
-            max = l
-            max_index += 1
-        else:
-            pass
-    return distinct_elements[max_index],max
-
-def absent_students(marks):
-    count = 0
-    for i in marks:
-        if i == -1:
-            count += 1
-        else:
-            pass
-    return count
-
-print("AVERAGE OF THE CLASS IS-\n",average(marks))
-print("MAXIMUM MARKS ARE-\n",maximum(marks))
-print("MINIMUM MARKS ARE-\n",minimum(marks))
-print("COUNT OF STUDENTS WHO ARE ABSENT IS-\n",absent_students(marks))
-result = max_frequency(marks)
-print("MAXIMUM OCCURING MARKS AND ITS FREQUENCY IS-\n",result[0],result[1])
+main()
+avg()
+max_score()
+min_score()
+frequent_score()
